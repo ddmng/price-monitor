e77398172c6df9a2b23d3603ac99fcc5bffac82e
ddmng <daniele.diminica@gmail.com>
2018-10-06 18:50:32 +0200

    Parametrized container

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,1 +1,19 @@
 #!/usr/bin/env bash
+
+KEY="$1"
+RET="0"
+case ${KEY} in
+    ci) # Runs ci tests
+        $(npm bin)/cypress run --spec 'cypress/integration/ci/**/*'
+    ;;
+    healthcheck) # Runs healthcheck
+        $(npm bin)/cypress run --spec 'cypress/integration/healthcheck/**/*'
+    ;;
+
+    *)    # unknown option
+        echo "Unrecognized command"
+    ;;
+esac
+
+echo "Exit code: "$RET
+exit $RET
