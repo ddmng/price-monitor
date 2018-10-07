951b6127b4fbf3b97b220bdf8f53baa3aa8893db
ddmng <daniele.diminica@gmail.com>
2018-10-07 09:33:18 +0200

    Fix entrypoint return code

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -5,12 +5,15 @@ RET="0"
 case ${KEY} in
     ci) # Runs ci tests
         $(npm bin)/cypress run --spec 'cypress/integration/ci/**/*'
+        RET="${?}"
     ;;
     healthcheck) # Runs healthcheck
         $(npm bin)/cypress run --spec 'cypress/integration/healthcheck/**/*'
+        RET="${?}"
     ;;
     *)    # unknown option
         echo "Unrecognized command"
+        RET=128
     ;;
 esac
 
