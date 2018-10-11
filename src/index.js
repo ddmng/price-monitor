ed30fd9fb8375efd6828665b9aabdc3a36eac859
ddmng <daniele.diminica@gmail.com>
2018-10-11 21:34:27 +0200

    Multichart

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import { app, h } from "hyperapp";
 
-console.log("starting");
-import * as firebase from "./fx/firebase";
+import * as Highcharts from "highcharts";
 
-import { Connect, ToFirebase, FromFirebase } from "./backend";
+import { Connect, FromFirebase } from "./backend";
 
 const blankState = {
   status: "idle",
@@ -16,18 +15,52 @@ const blankState = {
   data: []
 };
 
+const parsePrice = (priceString) => 
+  (priceString && priceString.split(' ').length)?
+    priceString.split(' ')[1]:""
+
+const Chart = ({state}) => {
+  if (state.data.length) {
+    let product = state.data[1]
+    Highcharts.chart("chart", {
+      title: {
+        text: state.data[1].name
+      },
+      series: [{
+        name: 'price',
+        data: Object.keys(product.prices).map(date => parseInt(parsePrice(product.prices[date].current)))
+      }],
+      xAxis: {
+        categories: Object.keys(product.prices)
+      },
+    });
+  }
+}
+
+const makeId = (name) => name.replace(' ', '_')
+
+const PriceChart = ({state, product}) => {
+    setTimeout(() => Highcharts.chart(makeId(product.name), {
+      title: {
+        text: product.name
+      },
+      series: [{
+        name: 'price',
+        data: Object.keys(product.prices).map(date => parseInt(parsePrice(product.prices[date].current)))
+      }],
+      xAxis: {
+        categories: Object.keys(product.prices)
+      },
+    }), 1000);
+}
 const Products = ({ state }) => (
   <div>
     <div>Products</div>
     {state.data.map(product => (
       <div>
         <div>{product.name}</div>
-        {Object.keys(product.prices).map(date => (
-          <div>
-            <div>date: {date}</div>
-            <div>price: {product.prices[date].current}</div>
-          </div>
-        ))}
+        <div id={makeId(product.name)}></div>
+        <PriceChart state={state} product={product} />
       </div>
     ))}
   </div>
